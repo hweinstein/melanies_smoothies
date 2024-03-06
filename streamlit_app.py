ce6cdf15a6af9b4d1f85786fc3cd83a7050d2a5a
hweinstein <hweinstein@finservconsulting.com>
2024-03-06 15:20:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-# Import python packages
+RCH# Import python packages
 import streamlit as st
 import snowflake.connector
 import requests
@@ -21,7 +21,7 @@ st.write('The name on your Smoothie will be:', name_on_order)
 
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 #st.dataframe (data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
