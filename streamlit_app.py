7d08a0748a8053a70bacfe598a81394494dbf8c3
hweinstein <hweinstein@finservconsulting.com>
2024-03-06 08:41:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+headers = {
+    "authorization": st.secrets["secrets.toml"]}
+    
+
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
